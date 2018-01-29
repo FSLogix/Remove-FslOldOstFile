f4f762704d290a4b470ab3a6b8614cf7e1f82946
Jim Moyle <jmoyle@fslogix.com>
2018-01-29 16:11:42 +0000

    initial structure

---
+++ Functions/Remove-FslOldOstFile.ps1
@@ -0,0 +1,22 @@
+function Remove-FslOldOstFile {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Position = 0,
+            Mandatory = $true)]
+        [String]$Text,
+        [Parameter(
+            Position = 1,
+            Mandatory = $true)]
+        [int]$Number
+    )
+    BEGIN {
+        Set-StrictMode -Version Latest
+    } #BEGIN
+    PROCESS {
+
+
+    } #PROCESS
+    END {
+    } #END
+}#function
